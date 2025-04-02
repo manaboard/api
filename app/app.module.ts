03037635e6106332df3e3c443c27eb965510ab4f
João Vitor de Jesus Oliveira <contato@joaovjo.com.br>
2025-04-01 22:52:10 -0300

    refactor: ajustar importações e remover arquivos obsoletos

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from '@/app.controller';
-import { AppService } from '@/app.service';
-import { DatabaseModule } from '@/core/database/database.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    DatabaseModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- app/core/interfaces/repository.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IBaseRepository<T, K> {
-  findAll(): Promise<T[]>;
-  findById(id: number | string): Promise<T | null>;
-  create(data: K): Promise<T>;
-  update(id: number | string, data: Partial<K>): Promise<T>;
-  delete(id: number | string): Promise<boolean>;
-}
